bd1ca12c845aa729586c2a6c1f8b0ad08ab7ff7e
Jonathan Johnson <jonathandila682@gmail.com>
2022-03-07 03:00:25 -0600

    refactoring cod

--- script.js
+++ script.js
@@ -8,10 +8,7 @@ const charAmountRange = document.getElementById('charAmountRange')
 const form = document.getElementById('passwordGeneratorForm')
 const password = document.getElementById('password')
 
-var lengthOfPassword = document.getElementById('lengthOfPassword')
-var char = "";
-
-
+const lengthOfPassword = document.getElementById('lengthOfPassword')
 //sync  charAmountNum and charAmountRange
 charAmountNum.addEventListener('input', syncCharAmount)
 charAmountRange.addEventListener('input', syncCharAmount)
@@ -23,109 +20,64 @@ function syncCharAmount(e) {
     value = lengthOfPassword
     return value;
   }
-
-// password values
+// password values, Lowercase, Uppercase, numbers, special characters
 const values = {
-    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXWZ",
-    lowerCase: "abcdefghijklmnopqrstuvwxyz",
-    numbers: "0123456789",
-    symbols: "!@#4567890-=<>?:{}|[]\~`"
+    upperCase: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+    lowerCase: ["abcdefghijklmnopqrstuvwxyz"],
+    numbers: ["0123456789"],
+    symbols: ["!@#$%^&*()_+{}|[]\:;'<>?,./~'"]
 }
-  //     b. Lowercase, upercase, numbers, special characters
-  const getValues = [
-        function upperCase() {
-          return values.upperCase[Math.floor(Math.random() * values.upperCase.length)]
-      }, 
-        function loweCase() {
-          return values.lowerCase[Math.floor(Math.random() * values.lowerCase.length)]
-      },
-        function number() {
-          return values.number[Math.floor(Math.random() * values.number.length)]
-      }, 
-        function symbols() {
-          return values.symbols[Math.floor(Math.random() * values.symbols.length)]
-      }
-
-    ];
-
- //Random password picking function            
-// function chooseRandomNumber(length, char) {
-//     for (let i = 0; i < charAmountNum; i++) {
-//     var password = char + possibleValues.charAt(Math.floor(Math.random() * Math.floor(possibleValues.length - 1)));
-
-//     } 
-//     //Add password to textbox
-//     document.getElementById("display").possibleValues = password;
-
-//  }
- 
+    // 2. Validate the input
+    // 3. Generate password based on criteria.Generate
 function generatePassword() {
-
+    let charset = '' //empty string of all the chars selected
     const chooseUpperCaseElement = document.getElementById('chooseUpperCase').checked
     const chooseLowerCaseElement = document.getElementById('chooseUpperCase').checked
     const chooseNumbersElement = document.getElementById('chooseNumbers').checked
     const chooseSymbolsElement = document.getElementById('chooseSymbols').checked
-    if(chooseUpperCaseElement + chooseLowerCaseElement + chooseNumbersElement + chooseSymbolsElement === 0){
-        alert('Please select length and one of the boxes below.');
+    if (chooseUpperCase + chooseLowerCase + chooseNumbers + chooseSymbols == 0) {
+        alert("Please select legth of password and criteria from boxes below")
         return;
     }
-    var passTextBox = document.querySelector("#password");
-    const length = document.getElementById('length')
-    char = "";
-
-    while (syncCharAmount.value > char.length) {
-        let addKey = getpass[Math.floor(Math.random() * getpass.length)]
-        let boxChecked = document.getElementById(addKey.name).checked;
-        if(boxChecked) {
-            password += addKey()
-        }
+    if (chooseUpperCaseElement) {
+        charset = charset + values.upperCase;
     }
-        
+    if (chooseLowerCaseElement) {
+        charset = charset + values.lowerCase;
     }
-    showPassword.innerHTML = password;
-// Add event listener to generate button
-
-
-            
-function showPassword(newPassWord) {
-
-    
-    passTextBox.value = newPassWord;
-
-    if(password) {
-        return;
+    if (chooseNumbersElement) {
+        charset = charset + values.numbers;
     }
-
+    if (chooseSymbolsElement) {
+        charset = charset + values.symbols;
+    }
+    charset = charset.split('')//turn string into an array
+    // add chars to charset based on user selection
+    charPasswd = "";
+    //iterate length times getting a random characters 
+    for (let i = 0; i < charPasswd.length; i++) {
+        var index = Math.floor((Math.random() * charset.length));
+        finalResult += charset[index];
+    }
+    password.values = charPasswd
 }
-generateBtn.addEventListener("click", showPassword);
-
-
-
-        //  console.log("Yess! You are clicking me!")
-        // let charCodes = Lowercase_char_codes
-        // if(includeUppercase) charCodes = charCodes.concat(Uppercase_char_codes)
-        // if(includeSymbols) charCodes = charCodes.concat(Symbol_char_codes)
-        // if(includeNumbers) charCodes = charCodes.concat(Number_char_codes)
-        // const passwordCharacter = []
-        // for (let i = 0; i < characterAmount; i++) {
-        //     const character = charCodes[Math.floor(Math.random() * charCodes.length)]
-        //     passwordCharacter.push(String.fromCharCode(character))
+//addEventListener to generate button
+// 4. Display password to the page.
+generateBtn.addEventListener("click", generatePassword);
 
 
 
 
 
 
-    // 1. Prompted the user for the password criteria
-    //     a. Password length 8 < 128
+
+
   
 
-    // 2. Validate the input
-    // 3. Generate password based on criteria.Generate
 
 
-    // 4. Display password to the page.
 
+    
 
 
 
